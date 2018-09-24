23b50873e1c78bf37eb0a037d4a52ab9a311f11e
Matheus Encarnação <desenvolvimentoequipe@outlook.com.br>
2018-09-24 14:10:07 -0300

    Criação controller e view de login

---
+++ TccUsjt2018/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TccUsjt2018.ViewModels.Login;
+
+namespace TccUsjt2018.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(LoginViewModel model)
+        {
+            if(model.Email == null || model.Email.Length == 0 || !model.Email.Contains("@"))
+            {
+                ModelState.AddModelError("Email", "E-mail inválido!");
+            }
+
+            if(model.Senha == null || model.Senha.Length < 6)
+            {
+                ModelState.AddModelError("Senha", "Senha inválida!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+    }
+}
