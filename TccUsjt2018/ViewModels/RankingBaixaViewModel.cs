1b79007f5812dcab9b2150748e6ff8be32be8193
Gean Alifi Coutrinho <gean.coutrinho@hotmail.com>
2018-12-17 21:00:43 -0200

    Ajustes corretivos

---
+++ TccUsjt2018/ViewModels/RankingBaixaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TccUsjt2018.ViewModels
+{
+    public class RankingBaixaViewModel
+    {
+        public string NomeProduto { get; set; }
+        public int QuantidadeBaixa { get; set; }
+    }
+}
